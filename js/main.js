1b4cd4aaa5e40a865da746c348381dfebc2329e7
Anatlijj <Argoon1@yandex.ru>
2021-04-21 15:44:49 +0300

    ADAPTIV

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-$(function(){
+$(function () {
 
-$('.carousel__inner').slick({
-  arrows: false,
-  dots: true,
-  slidesToShow: 3,
-});
+  $('.carousel__inner').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+    ]
+  });
 
 
 
